fdebbd65fbd225f5d4696585e3d8a4113b9f84a8
sirsamhain <joelandapophis@gmail.com>
2020-03-01 19:04:11 -0600

    g

--- MessagingMS/src/Main.ts
+++ MessagingMS/src/Main.ts
@@ -2,14 +2,14 @@ import express from "express";
 import bodyParser from "body-parser"
 import figlet from "figlet";
 import dotenv from "dotenv";
-import { Database } from "./services/Database";
+// import { Database } from "./services/Database";
 import { MessageController } from "./controller/MessageController";
 
 dotenv.config();
 import { Properties } from "./Properties"
 const app = express();
 app.use(bodyParser.json());
-Database.connect();
+// Database.connect();
 const port = Properties.PORT;
 
 //starting the controllers
